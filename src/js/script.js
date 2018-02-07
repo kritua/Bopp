64e3cc46922f954bf3cafeb37fc5a3b8b8a9d8bb
kritua <kritua2@gmail.com>
2018-02-07 23:18:42 +0300

    MUZ

--- src/js/script.js
+++ src/js/script.js
@@ -54,6 +54,59 @@ $(document).ready(function() {
             }
         })
     }
+
+    var labels = ['weeks', 'days', 'hours', 'minutes', 'seconds'],
+        nextYear = (new Date().getFullYear() + 1) + '/01/01',
+        currDate = '00:00:00:00:00',
+        nextDate = '00:00:00:00:00',
+        parser = /([0-9]{2})/gi,
+        $example = $('#main-example');
+    // Parse countdown string to an object
+    function strfobj(str) {
+        var parsed = str.match(parser),
+            obj = {};
+        labels.forEach(function(label, i) {
+            obj[label] = parsed[i]
+        });
+        return obj;
+    }
+    // Return the time components that diffs
+    function diff(obj1, obj2) {
+        var diff = [];
+        labels.forEach(function(key) {
+            if (obj1[key] !== obj2[key]) {
+                diff.push(key);
+            }
+        });
+        return diff;
+    }
+    // Starts the countdown
+    $example.countdown(nextYear, function(event) {
+        var newDate = event.strftime('%w:%d:%H:%M:%S'),
+            data;
+        if (newDate !== nextDate) {
+            currDate = nextDate;
+            nextDate = newDate;
+            // Setup the data
+            data = {
+                'curr': strfobj(currDate),
+                'next': strfobj(nextDate)
+            };
+            // Apply the new values to each node that changed
+            diff(data.curr, data.next).forEach(function(label) {
+                var selector = '.%s'.replace(/%s/, label),
+                    $node = $example.find(selector);
+                // Update the node
+                $node.removeClass('flip');
+                $node.find('.curr').text(data.curr[label]);
+                $node.find('.next').text(data.next[label]);
+                // Wait for a repaint to then flip
+                setTimeout(function($node) {
+                    $node.addClass('flip');
+                }, 50, $node);
+            });
+        }
+    });
 });
 
 
